745b2224e9949057ac5bc826a7858e021ee5d577
staskosh <staskoshenkov@rambler.ru>
2022-02-21 12:24:03 +0300

    Unpack book and author in the right way.

--- Offline_library.py
+++ Offline_library.py
@@ -27,13 +27,12 @@ def get_img_link(book_id):
     url = f'http://tululu.org/b{book_id}/'
     response = requests.get(url)
     response.raise_for_status()
-    #check_for_redirect(response)
+    check_for_redirect(response)
     soup = BeautifulSoup(response.text, 'lxml')
     img_path = soup.find('td', class_='ow_px_td')\
         .find('table', class_='d_book')\
         .find('div', class_='bookimage').find('img')['src']
     img_link = 'http://tululu.org' + img_path
-    print(img_link)
     return img_link
 
 
@@ -55,8 +54,7 @@ def parse_book_page(html_content):
     soup = BeautifulSoup(html_content.text, 'lxml')
     book_info = soup.find('td', class_='ow_px_td')
     book_and_author = book_info.find('h1').text
-    book = book_and_author.split('::')[0]
-    author = book_and_author.split('::')[1]
+    book, author = book_and_author.split('::')
     found_genres = book_info.find('span', class_='d_book').find_all('a')
     genres = get_genres(found_genres)
     found_comments = book_info.find_all('span', class_='black')
